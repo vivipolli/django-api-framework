e0af6dc9189a09d9780c1628e8f221a6db700289
vivipolli <pollivivian@gmail.com>
2019-03-13 00:43:36 -0300

    Configuring the app

--- telemedicina_test/settings.py
+++ telemedicina_test/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['telemedicina-test.herokuapp.com','localhost']
 
 
 # Application definition
